c2a26a3c21eaccd25aa6c6fbeed453c57aca3e6e
Josh Banzon <banzon3@marshall.edu>
2025-04-28 02:49:41 -0400

    Added sounds, added items, added item pickup window, lots of bug fixing, removed unused assets.

---
+++ Source/capstone/capstone.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "capstone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, capstone, "capstone" );
